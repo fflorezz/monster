0a0e238e3abfbeca7eb6b774a4daac24367baf87
fflorezz <felipeflorezart@gmail.com>
2020-04-01 16:47:56 -0500

    Added input search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
-
+import { SearchBox } from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
   async componentDidMount() {
@@ -16,11 +17,23 @@ class App extends Component {
     this.setState({ monsters: users });
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <h1>Mnsters Roledex</h1>
-        <CardList monsters={this.state.monsters}></CardList>
+        <h1>Monsters Roledex</h1>
+        <SearchBox
+          placeholder="search monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
